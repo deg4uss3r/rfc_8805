329e45d85aa3b6ed5946c59b7a35011e272d7e8a
Ricky Hosfelt <ricky_github@hosfe.lt>
2025-12-24 12:51:41 -0500

    Updated code documentation and added keywords to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -6,28 +6,69 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
+/// Errors that can happen when parsing the RFC 8805 format
 pub enum Error {
     #[error("Error parsing IP subnet {0}")]
+    /// There was an error parsing the String into a [`ipnet::IpNet`] type
     IpSubnetParse(String),
-    #[error("Erro parsing IP Address {0}")]
+    #[error("Error parsing IP Address {0}")]
+    /// There was an error parsing the String into a [`std::net::IpAddr`] type
     IpAddrParse(String),
 }
 
 #[derive(Debug, Deserialize)]
+/// A raw Record type, this is used mainly for deserialization or if you wanted to use your
+/// own types adn convert manually from Strings
+/// 
+/// A typical use case is to use `Record::try_from(rawRecord)` to convert this into a better format
+/// and take advantage of more information in the subnet and country code libraries utilized in the [`Record`] type
 pub struct RawRecord {
+    /// Representation of the IP subnet advertised
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.1
     ip: String,
+    /// If exists the 2 letter country code identifier (e.g. US)
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.2
     alpha2: Option<String>,
+    /// If exists the region in ISO 3166-2 format
+    /// 
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.3
     region: Option<String>,
+    /// If exists the city name in UTF8 character encoding (excluding the `,` character)
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.4
     city: Option<String>,
+    /// If exists the postal code in UTF8 character encoding (excluding the `,` character)
+    /// This filed is deprecated for newer geofeeds but might be present in order formats
+    /// 
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.5
     postal_code: Option<String>,
 }
 
 #[derive(Debug, Clone)]
+/// A enhanced view of a Record of RFC8805 format
 pub struct Record {
+    /// Representation of the IP subnet advertised
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.1
     ip: IpNet,
+    /// If exists the 2 letter country code identifier (e.g. US)
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.2
     alpha2: Option<CountryCode>,
+    /// If exists the region in ISO 3166-2 format
+    /// 
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.3
     region: Option<Subdivision>,
+    /// If exists the city name in UTF8 character encoding (excluding the `,` character)
+    ///
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.4
     city: Option<String>,
+    /// If exists the postal code in UTF8 character encoding (excluding the `,` character)
+    /// This filed is deprecated for newer geofeeds but might be present in order formats
+    /// 
+    /// https://datatracker.ietf.org/doc/html/rfc8805#section-2.1.1.5
     postal_code: Option<String>,
 }
 
@@ -57,30 +98,38 @@ impl TryFrom<RawRecord> for Record {
 }
 
 impl Record {
+    /// Get the advertised IP prefix of the record
     pub fn ip(self) -> IpNet {
         self.ip
     }
 
+    /// Get the country code, in alpha 2, of the record
     pub fn alpha2(self) -> Option<CountryCode> {
         self.alpha2
     }
 
+    /// Get the region (formally called country) of the record
     pub fn country(self) -> Option<CountryCode> {
         self.alpha2()
     }
 
+    /// Get the region of the record
     pub fn region(self) -> Option<Subdivision> {
         self.region
     }
 
+    /// Get the city of the record
     pub fn city(self) -> Option<String> {
         self.city
     }
 
+    /// Get the postal code of the record 
+    /// Postal codes have been deprecated but could still exist in feeds
     pub fn postal_code(self) -> Option<String> {
         self.postal_code
     }
 
+    /// Checks if the IP exists in the IP Prefix of the record
     pub fn check_ip(&self, input: &str) -> Result<bool, Error> {
         let input_ip: IpAddr = match input.parse() {
             Ok(i) => i,
@@ -92,6 +141,7 @@ impl Record {
 }
 
 #[derive(Debug, Serialize)]
+/// Easily serializable representation of [`rust_iso3166::CountryCode`]
 struct SerAlpha2 {
     name: String,
     alpha2: String,
@@ -100,6 +150,7 @@ struct SerAlpha2 {
 }
 
 #[derive(Debug, Serialize)]
+/// Easily serializable representation of [`rust_iso3166::iso3166_2::Subdivision`]
 struct SerRegion {
     name: String,
     subdivision_type: String,
@@ -109,6 +160,7 @@ struct SerRegion {
 }
 
 #[derive(Debug, Serialize)]
+/// A easily serializable version of [`Record`] mainly used for CLI applications to output in `json`
 pub struct SerRecord {
     ip: String,
     alpha2: Option<SerAlpha2>,
